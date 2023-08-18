8d59393f5747d92b34f2628ad1acce545af881a8
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-18 17:03:57 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -2,3 +2,8 @@ def solution(my_string, is_suffix):
     answer = 0
     a = [my_string[i:] for i in range(len(my_string))]
     return int(is_suffix in a)
+
+def solution(my_string, is_suffix):
+    if my_string[-len(is_suffix):] == is_suffix:
+        return 1
+    return 0
