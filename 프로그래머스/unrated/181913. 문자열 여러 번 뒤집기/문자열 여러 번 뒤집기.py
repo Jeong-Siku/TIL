b4145f142c33686415d3096f343e5c0c04a7d8e9
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-18 16:35:35 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 2.77 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -9,3 +9,11 @@ def solution(my_string, queries):
         my_string = my_string[:start_idx] + after + my_string[end_idx:]
         
     return my_string
+
+def solution(my_string, queries):
+    # 문자열의 순서는 바꿀 수 없다. 새로 초기화해야함
+    answer = list(my_string)
+    # 문자열을 리스트화시키면 한글자씩 원소로 만들어진다.
+    for s,e in queries:
+        answer[s:e+1] = answer[s:e+1][::-1]
+    return "".join(answer)
