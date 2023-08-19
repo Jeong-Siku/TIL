d0dc5e4270166996c9535ffeeff35f59344484fd
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-19 17:17:49 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -12,3 +12,15 @@ def solution(name, yearning, photo):
                photo_score.append(score[j])
         score_list.append(sum(photo_score))
     return score_list
+
+def solution(name, yearning, photo):
+    dic = dict(zip(name,yearning))
+    
+    score_list = []
+    for group in photo:
+        score = 0
+        for j in group:
+            if j in dic:
+               score+=dic[j]
+        score_list.append(score)
+    return score_list
