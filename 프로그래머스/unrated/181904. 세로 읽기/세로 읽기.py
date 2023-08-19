e4d254c68fb9809e2ecc3904f31fae9168077911
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-19 12:15:07 +0900

    [unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -0,0 +1,6 @@
+def solution(my_string, m, c):
+    answer = [my_string[i:i+m] for i in range(0,len(my_string),m)]
+    word = []
+    for i in answer:
+        word.append(i[c-1])
+    return "".join(word)
