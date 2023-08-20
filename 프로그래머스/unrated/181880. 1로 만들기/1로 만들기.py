e7c8cf07d70c4eafbeb0c83ddd186ffab9ee5e6f
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-21 02:24:38 +0900

    [unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,14 @@
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        while True:
+            if i==1:
+                break
+                
+            if i%2:
+                i = (i-1)/2
+                answer +=1
+            else:
+                i = i/2
+                answer +=1
+    return answer
