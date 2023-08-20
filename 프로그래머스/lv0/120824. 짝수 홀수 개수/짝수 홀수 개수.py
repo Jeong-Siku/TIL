ae00295882de9eea957b1db48ce6c2e32afc41b9
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-20 12:16:47 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -7,3 +7,9 @@ def solution(num_list):
         else:
             odd+=1
     return [even,odd]
+
+def solution(num_list):
+    answer = [0,0]
+    for i in num_list:
+        answer[i%2]+=1
+    return answer
