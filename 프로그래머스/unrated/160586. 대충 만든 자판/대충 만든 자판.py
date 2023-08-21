13fa043e022e3a3ec663aa232b327212c79ccdd3
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-22 00:22:51 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,6 +1,7 @@
 def solution(keymap, targets):
     answer = []
     
+    # 딕셔너리를 초기화 할 때 가능한 모든 값을 넣을 것
     # 딕셔너리로 각 알파벳 누르는 횟수를 저장하고 최종적으로 최소의 횟수만 남긴 뒤 타겟에서 해당 값을 더해서 구하려고 함
     # 딕셔너리 값이 최종 keymap의 것만 남음. 최소값을 어떻게 비교하지?
     prev = {i:100 for i in "".join(keymap)} 
@@ -16,6 +17,7 @@ def solution(keymap, targets):
         for a in tar:
             if a not in prev:
                 cnt=-1
+                # break는 해당 for문에만 영향을 끼친다.
                 break
             else:
                 cnt+=prev[a]
