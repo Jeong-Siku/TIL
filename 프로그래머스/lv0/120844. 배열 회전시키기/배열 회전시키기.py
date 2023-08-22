59371d38e764dc2755364d3fbc108e82495db3f9
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-22 12:20:31 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,6 +1,18 @@
 def solution(numbers, direction):
     answer=[]
     if direction=="right":
+        # 하나의 값은 리스트가 아니기에 병합할 수 없음.
+        # 리스트화 필요
         return [numbers[-1]]+numbers[:-1]
     else:
         return numbers[1:]+[numbers[0]]
+    
+from collections import deque
+
+def solution(numbers, direction):
+    numbers = deque(numbers)
+    if direction=="right":
+        numbers.rotate(1)
+    else:
+        numbers.rotate(-1)
+    return list(numbers)
