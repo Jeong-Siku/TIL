939f0f40f3c28a4410813f7490f3f5af6cdebee8
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-24 00:41:05 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -4,3 +4,7 @@ def solution(my_string):
         if i.isdigit():
             answer.append(int(i))
     return sorted(answer)
+
+import re
+def solution(my_string):
+    return sorted(map(int, (list(re.sub('[^0-9]', '', my_string)))))
