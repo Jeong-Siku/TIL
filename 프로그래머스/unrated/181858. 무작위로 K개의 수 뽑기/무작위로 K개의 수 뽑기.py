cdb81119554c89e35733d36649c55c590e3e9db3
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-23 17:09:01 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 11.60 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -14,3 +14,12 @@ def solution(arr, k):
             answer.append(-1)
 
     return answer
+
+def solution(arr, k):
+    answer = []
+    for i in arr:
+        if i not in answer:
+            answer.append(i)
+        if len(answer) ==k:
+            break
+    return answer+[-1]*(k-len(answer))
