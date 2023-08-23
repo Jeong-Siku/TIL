52fb6d4b6ca84a40561e0fa6aa874638a2f09cdc
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-23 18:11:29 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -14,3 +14,15 @@ def solution(n):
             result.append(i)
     
     return result
+
+def solution(n):
+    answer = []
+    d = 2
+    while n!=1:
+        if n%d==0:
+            if d not in answer:
+                answer.append(d)
+            n/=d
+        else:
+            d+=1
+    return answer
