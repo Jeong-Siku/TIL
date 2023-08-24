2ed9a7b5ac09bcfe64ac7b1f253ca738444b3a90
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-24 15:40:24 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -5,3 +5,7 @@ def solution(my_string):
             continue
         answer+=i
     return answer
+
+def solution(my_string):
+    # dict.fromkeys(iter,values)는 dict생성 메소드
+    return "".join(dict.fromkeys(my_string,0))
