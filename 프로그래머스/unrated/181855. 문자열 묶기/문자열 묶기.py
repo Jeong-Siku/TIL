0976db363a602d4a7e8641f820518196510328b3
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-24 14:54:06 +0900

    [unrated] Title: 문자열 묶기, Time: 20.34 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
@@ -2,6 +2,16 @@ def solution(strArr):
     ele_num = {i:0 for i in range(1,31)}
     for i in strArr:
         ele_num[len(i)]+=1
-    
-        
     return max(ele_num.values())
+
+def solution(strArr):
+    ele_num = {}
+    for i in strArr:
+        ele_num[len(i)] = ele_num.get(len(i),0)+1
+    return max(ele_num.values())
+
+def solution(strArr):
+    ele_num = [0]*31
+    for i in strArr:
+        ele_num[len(i)]+=1
+    return max(ele_num)
