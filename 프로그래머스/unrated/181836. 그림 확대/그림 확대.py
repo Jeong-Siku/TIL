b905b58e7221a953960b7f9c1caae45c8ffaf937
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-26 16:20:38 +0900

    [unrated] Title: 그림 확대, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
@@ -7,3 +7,10 @@ def solution(picture, k):
         for _ in range(k):
             answer.append(elem)
     return answer
+
+def solution(picture, k):
+    answer = []
+    for i in picture:
+        for _ in range(k):
+            answer.append(i.replace(".","."*k).replace("x","x"*k))
+    return answer
