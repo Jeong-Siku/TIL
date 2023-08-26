8326e33df5029815aee66aedd5aefdb8e81f7f51
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-26 17:36:14 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -4,3 +4,10 @@ def solution(arr):
         for j in range(len(arr)):
             answer.append(arr[i][j]==arr[j][i])
     return int(all(answer))
+
+def solution(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != arr[j][i]:
+                return 0
+    return 1
