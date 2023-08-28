a039e1c28c1fdba11cd1ed0b14da09cf911de13e
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-28 16:44:24 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -2,5 +2,7 @@ def solution(dots):
     dots.sort(key=lambda x:x[0])
     x = abs(dots[0][1]-dots[1][1])
     y = abs(dots[0][0]-dots[2][0])
-    
     return x*y
+
+def solution(dots):
+    return (max(dots)[0]-min(dots)[0])*(max(dots)[1]-min(dots)[1])
