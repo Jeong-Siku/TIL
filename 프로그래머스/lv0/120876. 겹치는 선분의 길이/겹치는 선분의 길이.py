b8b50205fe4a691db2eba8b142fd981a2d61dcd5
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-29 18:35:36 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -18,6 +18,11 @@ def solution(lines):
             overrap_point+=1
         if overrap_point>=2:
             answer+=1
-    
-    
     return answer
+
+def solution(lines):
+    s1 = set(i for i in range(lines[0][0], lines[0][1]))
+    s2 = set(i for i in range(lines[1][0], lines[1][1]))
+    s3 = set(i for i in range(lines[2][0], lines[2][1]))
+    # 집합의 교집합과 합집합을 이용할 것
+    return len((s1 & s2) | (s2 & s3) | (s1 & s3))
