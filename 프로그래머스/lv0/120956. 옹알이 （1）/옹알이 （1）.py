9af901c4bfc699cdd544f5238b616dbefdd9a025
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-30 02:32:15 +0900

    [level 0] Title: 옹알이 (1), Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -11,3 +11,14 @@ def solution(babbling):
         if i in result:
             answer+=1
     return answer
+
+import re
+def solution(babbling):
+    # []와 ()는 다르다. []는 해당 위치에서 일치할 수 있는 문자들의 집합
+    # ()는 일치하는 문자열을 묶어서 서브그룹으로 만들거나, 정규식을 그룹화하여 일치 여부를 확인하는 데 사용된다.
+    regex = re.compile('^(aya|ye|woo|ma)+$')
+    cnt=0
+    for i in babbling:
+        if regex.match(i):
+            cnt+=1
+    return cnt
