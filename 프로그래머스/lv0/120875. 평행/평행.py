aa09df3acb9e8f0d2afe89d3efe4b64b7d29962f
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-29 15:21:19 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -0,0 +1,16 @@
+def solution(dots):
+   #dots의 숫자를 직접 변수에 대입
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
+    
+    gradient_12 = abs((y2 - y1) / (x2 - x1))
+    gradient_34 = abs((y4 - y3) / (x4 - x3))
+    
+    gradient_14 = abs((y4 - y1) / (x4 - x1))
+    gradient_23 = abs((y3 - y2) / (x3 - x2))
+    
+    gradient_13 = abs((y3 - y1) / (x3 - x1))
+    gradient_24 = abs((y4 - y2) / (x4 - x2))
+    if gradient_12 == gradient_34 or gradient_23 == gradient_14 or gradient_13 == gradient_24:
+        return 1
+    return 0
+            
