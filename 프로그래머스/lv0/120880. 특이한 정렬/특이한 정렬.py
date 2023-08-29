473482340140d18a286a6077982aacba29f84ecc
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-30 01:13:31 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -0,0 +1,12 @@
+def solution(numlist, n):
+    dist=[]
+    numlist.sort(reverse=True)
+    for i in numlist:
+        dist.append(abs(n-i))
+    
+    dic = {numlist[idx]:dis for idx,dis in enumerate(dist)}
+    
+    # 정렬 기준에 -를 붙이면 reverse 기준이 붙는다.
+    ordered_dic = sorted(dic.items(),key=lambda x:(x[1],-x[0]))
+    
+    return [i for i,a in ordered_dic]
