b6060b98852463c3e585a2978c04b06cb1cf0356
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-08-30 17:32:49 +0900

    [level 0] Title: k의 개수, Time: 8.97 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -7,5 +7,5 @@ def solution(i, j, k):
         cnt+=str(i).count(str(k))
     return cnt
 
-# 1 2 3 4 5 6 7 8 9 
-# 10 11 12 13 14 15 16 17 18 19
+def solution(i, j, k):
+    return sum(str(i).count(str(k)) for i in range(i,j+1))
