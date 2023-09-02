ca089d3ebf3b49799a888f1e6aa7d564e6327f55
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-02 18:15:30 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.63 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -2,9 +2,21 @@ def solution(s):
     answer = []
     # 리스트
     # 인덱스
+    # 같은 원소의 *마지막* 인덱스 값 => dict의 초기화 이용가능
     for idx,i in enumerate(s):
         if s.index(i) == idx:
             answer.append(-1)
         else:
             answer.append(s[:idx][::-1].index(i)+1)
+    return answer
+
+def solution(s):
+    answer = []
+    dic = {}
+    for idx,j in enumerate(s):
+        if j not in dic:
+            answer.append(-1)
+        else:
+            answer.append(idx-dic[j])
+        dic[j]=idx
     return answer
