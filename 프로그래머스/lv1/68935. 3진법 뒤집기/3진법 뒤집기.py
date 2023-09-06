b11156278246ffef9ee1f4681b145e3811bc74f6
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-06 15:21:00 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = []
+    while n!=0:
+        answer.append(n%3)
+        n//=3
+    
+    result=0
+    for idx,num in enumerate(answer):
+        result+=answer[::-1][idx]*(3**idx)
+        
+    return result
