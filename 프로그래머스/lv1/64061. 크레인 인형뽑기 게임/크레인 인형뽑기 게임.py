0bc0efbaee1720d4faf3c387abb164e6c82ef94f
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-06 23:44:18 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.99 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -20,3 +20,19 @@ def solution(board, moves):
             result.pop()
             answer+=2
     return answer
+
+def solution(board, moves):
+    result=[]
+    answer=0
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                result.append(board[j][i-1])
+                # 끝난 보더라인은 초기화시킨다.
+                board[j][i-1]=0
+                
+                if len(result)>1 and result[-1]==result[-2]:
+                    result=result[:-2]
+                    answer+=2
+                break
+    return answer
