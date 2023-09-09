93fe08fcd920956326d4a428c849f0ae6daaabec
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-09 14:00:21 +0900

    [level 1] Title: 모의고사, Time: 3.52 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -14,7 +14,7 @@ def solution(answers):
             method_2_cnt+=1
         if method_3[idx] == i:
             method_3_cnt+=1
-            
+             
     dic = {1:method_1_cnt,2:method_2_cnt,3:method_3_cnt}
 
     result=[]
@@ -23,3 +23,24 @@ def solution(answers):
             print(dic[i])
             result.append(i)
     return result
+
+def solution(answers):
+    method_1 = [1,2,3,4,5]
+    method_2 = [2,1,2,3,2,4,2,5]
+    method_3 = [3,3,1,1,2,2,4,4,5,5]
+    cnt = [0,0,0]
+    
+    for idx,answer in enumerate(answers):
+        if answer == method_1[idx%len(method_1)]:
+            cnt[0]+=1
+        if answer == method_2[idx%len(method_2)]:
+            cnt[1]+=1
+        if answer == method_3[idx%len(method_3)]:
+            cnt[2]+=1
+    
+    result = []
+    for idx,i in enumerate(cnt):
+        if i == max(cnt):
+            result.append(idx+1)
+    return result
+
