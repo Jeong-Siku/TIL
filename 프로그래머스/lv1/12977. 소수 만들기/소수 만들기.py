575fc35c937bef1c40f20db3f14dfeb9ef56132b
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-11 19:32:51 +0900

    [level 1] Title: 소수 만들기, Time: 236.69 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,20 @@
+def solution(nums):
+    nums.sort()
+    
+    mod_2 = []
+    for i in range(2,3001):
+        list_1 = []
+        for j in range(1,i+1):
+            if i%j==0:
+                list_1.append(j)
+        if len(list_1)==2:
+            mod_2.append(i)
+    
+    result = 0
+    for i in range(len(nums)-2):
+        for j in range(i+1,len(nums)-1):
+            for k in range(j+1,len(nums)):
+                if nums[i]+nums[j]+nums[k] in mod_2:
+                    result+=1
+        
+    return result
