80dd39496fe784e549357b4a6d59497a2d1ce56a
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-12 18:00:56 +0900

    [level 1] Title: 시저 암호, Time: 2.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -3,8 +3,17 @@ def solution(s, n):
     for i in s:
         if ord(i)>=97:
             answer+=chr(97+(ord(i)+n-97)%26)
-        elif i==" ":
-            answer+=" "
         else:
             answer+=chr(65+(ord(i)+n-65)%26)
+    return answer
+
+def solution(s, n):
+    answer=""
+    for i in s:
+        if i.isupper():
+            answer+=chr((ord(i)-ord("A")+n)%26+ord("A"))
+        elif i.islower():
+            answer+=chr((ord(i)-ord("a")+n)%26+ord("a"))
+        else:
+            answer+=i
     return answer
