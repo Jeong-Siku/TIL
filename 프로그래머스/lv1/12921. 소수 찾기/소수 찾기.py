5c93eda44e7ab2600d4f4799bc0dea9bc2b4da91
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-12 18:46:04 +0900

    [level 1] Title: 소수 찾기, Time: 287.96 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -7,3 +7,19 @@ def solution(n):
         else:
             answer+=1
     return answer
+
+# 에라토스테네스의 체
+def solution(n):
+    list_n = [True for _ in range(n+1)]
+    answer = 0
+    for i in range(2,int(n**0.5)+1):
+        if list_n[i] ==True:
+            j=2
+            while i*j<=n:
+                list_n[i*j]=False
+                j+=1
+    for j in range(2,n+1):
+        if list_n[j]:
+            answer+=1
+    return answer
+                
