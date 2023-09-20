bcfb15123349028f5cc5efdeabb864deb77ede9c
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-20 20:00:32 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,5 +1,13 @@
 -- 코드를 입력하세요
+
+# 오라클
 SELECT truncate(PRICE,-4) as PRICE_GROUP, count(PRICE) as PRODUCTS
 from PRODUCT
 group by PRICE_GROUP
 order by PRICE_GROUP
+
+# # mysql
+# SELECT (PRICE-PRICE%10000) as PRICE_GROUP, count(PRICE) as PRODUCTS
+# from PRODUCT
+# group by PRICE_GROUP
+# order by PRICE_GROUP
