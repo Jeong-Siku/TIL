1beb8b113a26753c423b223d9d8208755de0d0d7
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-09-20 20:40:18 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -3,8 +3,21 @@ select FOOD_TYPE,REST_ID,REST_NAME,FAVORITES
 from (
     SELECT *,
         sum(FAVORITES) as best,
+    # over의 순서는 partition by 다음 order by
+    # over()에서 ,는 쓰지 않는다.
         rank() over(partition by FOOD_TYPE order by sum(FAVORITES) desc) as ranking
     from REST_INFO
     group by REST_NAME,FOOD_TYPE
     order by FOOD_TYPE desc,best desc) C
+where ranking = 1;
+
+# rank를 사용하면 굳이 groupby를 사용하지않아도 된다.
+# 가장 큰수를 구할때는 max를 사용해도 됨
+select FOOD_TYPE,REST_ID,REST_NAME,FAVORITES
+from (
+    SELECT *,
+        rank() over(partition by FOOD_TYPE order by FAVORITES desc) as ranking
+    from REST_INFO
+    ) C
 where ranking = 1
+order by FOOD_TYPE desc;
