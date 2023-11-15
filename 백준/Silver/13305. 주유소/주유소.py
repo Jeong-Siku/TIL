c3fca8c57090d64725ca4f85be2ac90c4c804d6b
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2023-11-16 07:30:33 +0900

    [Silver III] Title: 주유소, Time: 124 ms, Memory: 45892 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,18 +2,18 @@ import sys
 input = sys.stdin.readline
 n = int(input().rstrip())
 len_road = list(map(int,input().rstrip().split()))
-price = list(map(int,input().rstrip().split()))
-# 가장 가격이 낮은 도시에서 남은 거리의 기름을 다 산다.
-min_price = sorted(price)[1]
+price = list(map(int,input().rstrip().split()))[:-1]
+
+min_price = min(price)
 
 total_price = 0
-total_len = sum(len_road)
-for i in range(len(len_road)):
-    if price[i] != min_price:
-        total_price += price[i]*len_road[i]
-        total_len -= len_road[i]
-    else:
-        total_price += price[i]*total_len
+for i in range(n-1):
+    if price[i] == min_price:
+        total_price += price[i]*sum(len_road[i:])
         break
+    if price[i]<price[i+1]:
+        price[i+1]=price[i]
     
+    total_price += price[i]*len_road[i]
+
 print(total_price)
