383ae89e9003eaaa089a5caa28733eaea2fed09a
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2024-01-03 22:27:48 +0900

    chore : 파일 위치 변경

--- TIL_Kafka/01_consumer.py
+++ TIL_Kafka/01_consumer.py
@@ -1,14 +1,15 @@
 from kafka import KafkaConsumer
 
-BROKER_SERVERS = ["15.152.234.229:9092"]
+BROKER_SERVERS = ["13.208.235.178:9092"]
 TOPIC_NAME = "sample_topic"
 
 consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=BROKER_SERVERS)
 
-# Consumer는 파이썬의 Generator로 구현됨
-print("Wait....")
+# Consumer는 파이썬의 Generator로 구현됨. next 실행 전까지는 block 상태
+print("Wait...")
 
-for message in consumer:
+# 컨슈머는 항상 LISTEN(대기) 상태. Generator와 같이 topic에 정보가 들어오기 전까지 block 상태
+for message in consumer:  # topic에 정보가 들어오면 실행한다.
     print(message)
 
-print("Done....")
+print("Done...")  # 종료하기 전까지 이 부분은 뜨지 않는다.
--- TIL_Kafka/01_producer.py
+++ TIL_Kafka/01_producer.py
@@ -1,16 +1,19 @@
 from kafka import KafkaProducer
 
-# 브로커 목록을 정의
-#  브로커가 한개만 있다 하더라도 리스트 형태로 정의하는 것이 좋다.
-#  여러 개의 브로커가 띄워진 경우에도 한 개의 브로커만 입력해도 연결은 가능하나, 모두 적어주는 것이 좋다.
-BROKER_SERVERS = ["15.152.234.229:9092"]
+# 데이터를 보낼 브로커 목록을 정의.
+#   브로커가 한개만 있다 하더라도 리스트 형태로 정의하는 것이 좋다.
+#   여러 개의 브로커가 띄워진 경우에도 한개의 브로커만 입력해도 연결은 가능하나, 모두 적어주는 것이 좋다.
+#   장애 대응을 위해서!
+BROKER_SERVERS = ["13.208.235.178:9092"]
 TOPIC_NAME = "sample_topic"
 
 # 프로듀서 생성
 producer = KafkaProducer(bootstrap_servers=BROKER_SERVERS)
+print(producer)
 
 # 메시지를 토픽에 전송
-producer.send(TOPIC_NAME, b'Hello Kafka Python')
+producer.send(TOPIC_NAME, b"Hello Kafka Python")  # 용량을 줄이기 위해 바이트 문자열로 전송 b
 
 # 버퍼 스트림 제거(선택 사항이지만 하는 것이 좋다.)
+# 버퍼 스트림을 사용하면 마지막에 찌꺼기가 남는 경우가 생김. 이 경우를 예방하기 위해 사용
 producer.flush()
