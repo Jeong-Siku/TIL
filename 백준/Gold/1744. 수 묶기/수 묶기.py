c59cadeef93e2870b658b0795c63ba463dfa950b
Jeong-Siku <127370725+Jeong-Siku@users.noreply.github.com>
2024-02-01 20:21:56 +0900

    [Gold IV] Title: 수 묶기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,50 +1,31 @@
 n = int(input())
-p_arr = []
-m_arr = []
-z_arr = []
-o_arr = []
+arr = []
 for _ in range(n):
-    num = int(input())
-    if num>1:
-        p_arr.append(num)
-    elif num<0:
-        m_arr.append(num)
-    elif num==1:
-        o_arr.append(1)
-    else:
-        z_arr.append(0)
+    arr.append(int(input()))
     
-p_arr.sort()
-m_arr.sort()
+ans = arr.count(1)
 
-# 양수 : 개수 짝,홀인지 최소값이 1인지
-# 음수 : 개수 짝,홀인지 0이 존재하는지
-ans = 0
-if len(p_arr)<2:
-    ans+=sum(p_arr)
-elif len(p_arr)%2==0 :
-    for i in range(0,len(p_arr),2):
-        ans += p_arr[i]*p_arr[i+1]
-elif len(p_arr)%2==1 :
-    ans+=p_arr[0]
-    for i in range(1,len(p_arr),2):
-        ans += p_arr[i]*p_arr[i+1]
+m = [i for i in arr if i<=0]
+p = [i for i in arr if i>1]
+p.sort()
+m.sort(reverse=True)
 
-ans+=sum(o_arr)
-
-if len(m_arr)<2 and z_arr:
-    pass
-elif len(m_arr)<2 and not z_arr:
-    ans+=sum(m_arr)
-elif len(m_arr)%2==0 :
-    for i in range(0,len(m_arr),2):
-        ans += m_arr[i]*m_arr[i+1]
-elif len(m_arr)%2==1 and z_arr:
-    for i in range(0,len(m_arr)-1,2):
-        ans += m_arr[i]*m_arr[i+1]
-elif len(m_arr)%2==1 and not z_arr:
-    ans+=m_arr[-1]
-    for i in range(0,len(m_arr)-1,2):
-        ans += m_arr[i]*m_arr[i+1]
-
+while len(p)>=1:
+    if len(p)>=2:
+        a = p.pop()
+        b = p.pop()
+        ans+=a*b
+    elif len(p)==1:
+        a = p.pop()
+        ans+=a
+        
+while len(m)>=1:
+    if len(m)>=2:
+        a = m.pop()
+        b = m.pop()
+        ans+=a*b
+    elif len(m)==1:
+        a = m.pop()
+        ans+=a
+        
 print(ans)
